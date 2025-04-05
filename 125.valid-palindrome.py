613d3b6e682791e1738442a30c88e2ddc5f2ce59
yashsachdev <yashsachdev5677@gmail.com>
2025-04-05 20:52:15 +0530

    Valid palindrome code python

---
+++ 125.valid-palindrome.py
@@ -0,0 +1,83 @@
+#
+# @lc app=leetcode id=125 lang=python
+#
+# [125] Valid Palindrome
+#
+# https://leetcode.com/problems/valid-palindrome/description/
+#
+# algorithms
+# Easy (50.40%)
+# Likes:    10196
+# Dislikes: 8529
+# Total Accepted:    4M
+# Total Submissions: 8M
+# Testcase Example:  '"A man, a plan, a canal: Panama"'
+#
+# A phrase is a palindrome if, after converting all uppercase letters into
+# lowercase letters and removing all non-alphanumeric characters, it reads the
+# same forward and backward. Alphanumeric characters include letters and
+# numbers.
+# 
+# Given a string s, return true if it is a palindrome, or false otherwise.
+# 
+# 
+# Example 1:
+# 
+# 
+# Input: s = "A man, a plan, a canal: Panama"
+# Output: true
+# Explanation: "amanaplanacanalpanama" is a palindrome.
+# 
+# 
+# Example 2:
+# 
+# 
+# Input: s = "race a car"
+# Output: false
+# Explanation: "raceacar" is not a palindrome.
+# 
+# 
+# Example 3:
+# 
+# 
+# Input: s = " "
+# Output: true
+# Explanation: s is an empty string "" after removing non-alphanumeric
+# characters.
+# Since an empty string reads the same forward and backward, it is a
+# palindrome.
+# 
+# 
+# 
+# Constraints:
+# 
+# 
+# 1 <= s.length <= 2 * 10^5
+# s consists only of printable ASCII characters.
+# 
+# 
+#
+
+# @lc code=start
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        start=0
+        end = len(s)-1
+        while(start<end):
+            while(start<end and not s[start].isalnum()):
+                start +=1
+            while(end>start and not s[end].isalnum()):
+                end -= 1
+            if(s[start].lower() != s[end].lower()):
+                return False
+            start,end= start+1,end-1
+        return True
+                
+                
+        
+# @lc code=end
+
